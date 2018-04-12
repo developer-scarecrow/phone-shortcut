d0b8abf52ee609f5464e814799d945f891f8be78
yoshizaki <yoshizaki>
2018-04-12 10:12:27 +0900

    add script

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+var elem = document.querySelector('.fixed-action-btn');
+var instance = M.FloatingActionButton.init(elem, {
+    direction: 'top'
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton();
+});
